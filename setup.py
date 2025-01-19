3d521d9ae6b3833786ee5b4cec0bad2eabc921e3
Lokesh Garg <lgarg092@gmail.com>
2025-01-19 19:38:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='AI Medical Assistance',
+    version='0.0.0',
+    author='Lokesh',
+    author_email="lgarg092@gmail.com",
+    packages=find_packages(),
+    install_requires= []
+)
